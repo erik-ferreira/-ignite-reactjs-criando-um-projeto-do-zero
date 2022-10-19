c174f9aec7f20acd939fa7b95e21342fc47a91d6
erik-ferreira <erikdesouzagf@gmail.com>
2022-10-18 22:41:27 -0300

    create structure post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,10 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  // TODO
+  return <h1>Home</h1>;
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
