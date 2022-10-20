ce0017458a03f210c6cd8b81bf2f3b0bb33fd786
erik-ferreira <erikdesouzagf@gmail.com>
2022-10-19 23:46:34 -0300

    fix

---
+++ .slicemachine/assets/slices/TextBlog/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlog';
+
+export default {
+  title: 'slices/TextBlog'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Captured","spans":[]}],"description":[{"type":"paragraph","text":"Minim occaecat aliqua ullamco magna enim duis fugiat nulla fugiat dolor.","spans":[]}]},"slice_type":"text_blog","id":"_Default"}} />
+_Default.storyName = ''
